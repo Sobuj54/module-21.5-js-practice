3f2fbad9c0b6877a6a45d31760ba2d80f9ae36dc
sobuj <sobujahmed124@gmail.com>
2023-01-31 23:39:47 +0600

    largest

---
+++ biggestNumerInThree.js
@@ -0,0 +1,12 @@
+// largest number among three numbers'
+
+var x = 45
+var y = 1
+var z = 2
+if (x > y && x > z) {
+  console.log('x is largest')
+} else if (y > x) {
+  console.log('y is largest')
+} else {
+  console.log('z is largest')
+}
